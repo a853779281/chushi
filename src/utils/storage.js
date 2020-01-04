6e54c2d2d3b729664127b5d77ed27704bb0dbd25
jiang <1049895015@qq.com>
2020-01-04 11:48:48 +0800

    1-4am

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -2,6 +2,10 @@ export const getStorage = (name) => {
     return localStorage.getItem(name) && JSON.parse(localStorage.getItem(name)) || []
 }
 
-export const setStorage = (name,value) => {
+export const setStorage = (name, value) => {
     return localStorage.setItem(name, JSON.stringify(value))
+}
+
+export const removeStorage = (name) => {
+    return localStorage.removeItem(name)
 }
